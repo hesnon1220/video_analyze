2293d66b43e222517f16fecfaa1cc30f9101e66a
hesnon1220 <hesnon1220@gmail.com>
2023-02-17 19:11:52 +0900

    迴圈

--- draw_line.py
+++ draw_line.py
@@ -11,22 +11,27 @@ from Helper import shorten_number
 
 def main():
 
+    data_path = r"output"
+    data_list = os.listdir(data_path)
 
-    var_hist = []
+    output_path = r"img"
+    for i in tqdm(data_list) :
+        pic_name = i.replace("txt","")
+        var_hist = []
 
-    with open(r"output\hist_var.txt","r") as txtfile :
-        lines = txtfile.readlines()
-        for i in lines :
-            var_hist.append(eval(i))
-    
-    print(var_hist)
+        with open(os.path.join(data_path,i),"r") as txtfile :
+            lines = txtfile.readlines()
+            for i in lines :
+                var_hist.append(eval(i))
+        
+        #print(var_hist)
 
-    x_bar = np.arange( 0 , len( var_hist ) , 1 )
-    fig = plt.figure()
-    plt.subplot(111)
-    plt.plot( x_bar , var_hist , c = "r" )
-    plt.savefig(os.path.join(r"output\var_hist.png"),bbox_inches='tight',pad_inches = 0)
-    plt.close('all')
+        x_bar = np.arange( 0 , len( var_hist ) , 1 )
+        fig = plt.figure(figsize=(20,8))
+        plt.subplot(111)
+        plt.plot( x_bar , var_hist , c = "r" )
+        plt.savefig(os.path.join(output_path,"{}.png".format(pic_name)),bbox_inches='tight',pad_inches = 0)
+        plt.close('all')
 
 if __name__ == "__main__" :
     main()
--- main.py
+++ main.py
@@ -13,42 +13,44 @@ def main():
     video_list = os.listdir(data_floder)
     print(video_list)
     
+    for i in tqdm(video_list) :
+        video_path = os.path.join(data_floder,i)
+        video_name = i.replace(".mp4","")
+        vidCap = cv2.VideoCapture(video_path)
+        #fps = 23
+        loop_num = 1
+        frame_num = 0
+        stop_num = 0
 
-    video_path = os.path.join(data_floder,video_list[1])
-    vidCap = cv2.VideoCapture(video_path)
-    fps = 10
-    loop_num = 10
-    frame_num = 0
-    stop_num = 0
-
-    hist_list = []
+        hist_list = []
 
 
-    while True :
-        ret = vidCap.grab()
-        if not ret : break
-        #if stop_num == 36000 : break
-        if frame_num == 0 :
-            start_time = time.time()
-            ret,image = vidCap.retrieve()
-            #tmp_img = cv2.resize(image,[256,256])
-            hist_list.append(get_hist(image))
-            stop_num += 1
-            end_time = time.time()
-            print("process : {} --- {:.2f} it/s".format(stop_num,1/(end_time-start_time)),end="\r")
-        frame_num = ( frame_num + 1 ) % loop_num
+        while True :
+            ret = vidCap.grab()
+            if not ret : break
+            #if stop_num == 36000 : break
+            if frame_num == 0 :
+                start_time = time.time()
+                ret,image = vidCap.retrieve()
+                #tmp_img = cv2.resize(image,[256,256])
+                hist_list.append(get_hist(image))
+                stop_num += 1
+                end_time = time.time()
+                print(" {} process : {} --- {:.2f} it/s".format(video_name,stop_num,1/(end_time-start_time)),end="\r")
+            frame_num = ( frame_num + 1 ) % loop_num
         
     
 
-    #hist_var = []
-    #for i in range(len(hist_list)-1) :
-        #hist_var.append(hist_similar(hist_list[i],hist_list[i+1]))
-    #print(hist_var)
+        #hist_var = []
+        #for i in range(len(hist_list)-1) :
+            #hist_var.append(hist_similar(hist_list[i],hist_list[i+1]))
+        #print(hist_var)
 
-    with open(os.path.join(output_floder,"hist_var.txt"),"w") as txtfile:
-        for i in range(len(hist_list)-1) :
-            print(hist_similar(hist_list[i],hist_list[i+1]),file=txtfile)
-        
+        with open(os.path.join(output_floder,"{}_hist_var.txt".format(video_name)),"w") as txtfile:
+            for i in range(len(hist_list)-1) :
+                print(hist_similar(hist_list[i],hist_list[i+1]),file=txtfile)
+        vidCap.release()
+        cv2.destroyAllWindows()
 
 
 
