695c80c8d537bb2bf51c3993f0de0b72dee2bd10
hesnon1220 <hesnon1220@gmail.com>
2023-02-17 17:33:53 +0800

    事項

--- draw_line.py
+++ draw_line.py
@@ -6,8 +6,27 @@ import numpy as np
 from tqdm import tqdm
 from Helper import shorten_number
 
+
+
+
 def main():
+
+
+    var_hist = []
+
+    with open(r"output\hist_var.txt","r") as txtfile :
+        lines = txtfile.readlines()
+        for i in lines :
+            var_hist.append(eval(i))
     
+    print(var_hist)
+
+    x_bar = np.arange( 0 , len( var_hist ) , 1 )
+    fig = plt.figure()
+    plt.subplot(111)
+    plt.plot( x_bar , var_hist , c = "r" )
+    plt.savefig(os.path.join(r"output\var_hist.png"),bbox_inches='tight',pad_inches = 0)
+    plt.close('all')
 
 if __name__ == "__main__" :
     main()
